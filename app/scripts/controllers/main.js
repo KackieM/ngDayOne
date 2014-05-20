bec75ec5e5fa9b8dc32e28c927b256210934a3dd
Katherine Mattison <kbmedisto@gmail.com>
2014-05-20 07:45:14 -0400

    do it again

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('ngDayOneApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  })
+  .controller('ArtController', function($scope) {
+	this.product = art;
+
+	var art = {
+		name: 'Ball of Fire',
+		price: 2000,
+		description: 'This is a Ball of Fire',
+		
+	}
+
+
+
+
+
+
+  		// $scope.whateverYouWant = [
+  		// 	{
+  		// 		name: "Kat Mattison",
+  		// 		hobby: "reading"
+  		// 	},
+  		// 	{
+  		// 		name: "Calvin Webster",
+  		// 		hobby: "surfing"
+  		// 	},
+  		// 	{},
+  		// 	{},
+  		// 	{},
+
+  		// ];
+  });
